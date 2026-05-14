d54dd8cfa191f9c78d331680931cc03292069f1b
roohullah802 <jaanroohullah83@gmail.com>
2026-05-13 21:50:28 -0700

    feat: initialize backend server with Express, MongoDB, and Inngest and add frontend gitignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,8 +13,6 @@ import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-const __dirname = path.resolve();
-
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
@@ -29,14 +27,6 @@ app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-// make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 const startServer = async () => {
   try {
